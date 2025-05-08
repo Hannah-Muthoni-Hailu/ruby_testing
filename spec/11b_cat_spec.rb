6417d6e0dd7ab5e414cd23e4322a0662dcab3327
Hannah-Muthoni-Hailu <muthonihannahhailu@gmail.com>
2025-05-08 13:14:54 +0300

    Shared examples completed (did Cat)

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,21 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
+  subject(:wasabi) { described_class.new('Wasabi') }
 
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when both Cat and Dog have the talk function' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(wasabi).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(wasabi).to be_hiding
   end
 end
